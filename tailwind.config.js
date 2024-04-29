989504e9e7859165cb253e5560b9d778d2751caa
Tamanna Chadni <tamanna.chadni.eng@gmail.com>
2024-04-29 17:26:17 +0600

    themeChange set

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,14 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
 }
 
 
